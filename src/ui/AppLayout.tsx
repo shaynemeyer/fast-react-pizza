cb6e0d49747480fd52a799161daaa6cb358e25d4
Shayne Meyer <shayne.meyer@gmail.com>
2023-07-05 16:33:53 -0700

    Added Styles for input fields

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -9,7 +9,8 @@ function AppLayout() {
 
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      {isLoading && <Loader />}
+      {/* {isLoading && <Loader />} */}
+      {true && <Loader />}
       <Header />
 
       <div>
