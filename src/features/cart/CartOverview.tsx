d183458d2a8df8a6f35b66c50ea2a683b24106be
Shayne Meyer <shayne.meyer@gmail.com>
2023-07-06 07:06:00 -0700

    updates to cart overview

--- src/features/cart/CartOverview.tsx
+++ src/features/cart/CartOverview.tsx
@@ -8,6 +8,7 @@ function CartOverview() {
   const totalCartPrice = useSelector(getTotalCartPrice);
 
   if (!totalCartQuantity) return null;
+
   return (
     <div className="bg-stone-800 text-stone-200 text-sm md:text-base uppercase px-4 py-4 sm:px-6 flex items-center justify-between">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
